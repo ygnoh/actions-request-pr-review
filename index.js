eb255e7d80172fb62019815e02343495b2bd56d3
yonggoo-noh <yonggoo.noh@navercorp.com>
2022-03-18 10:51:11 +0900

    install axios and use it

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const axios = require("axios");
 
 try {
-    const authFetch = (url, init) => fetch(
-        url,
-        {
-            headers: {
-                Authorization: `token ${core.getInput("token")}`
-            },
-            method: "get",
-            ...init
-        }
-    );
+    const authFetch = url => axios({
+        method: "get",
+        headers: {
+            Authorization: `token ${core.getInput("token")}`
+        },
+        url
+    });
     const fetchPRs = () => authFetch(`${github.context.payload.repository.url}/pulls`);
 
     console.log(fetchPRs());
