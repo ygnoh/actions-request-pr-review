48eb032d42ee4b24a6a4ff80c0e5e9cfb3696d58
yonggoo-noh <yonggoo.noh@navercorp.com>
2022-04-18 15:59:00 +0900

    Store as Pull instances

--- index.js
+++ index.js
@@ -9,36 +9,50 @@ const authFetch = url => axios({
     },
     url
 }).then(res => res.data);
-const userUrlEmailPair = {};
+
+class Pull {
+    constructor(pullInfo) {
+        const {title, html_url, number} = pullInfo;
+
+        this._title = title;
+        this._url = html_url;
+        this._number = number;
+        this._reviewers = [];
+    }
+
+    get number() {
+        return this._number;
+    }
+
+    addReviewer(reviewer) {
+        this._reviewers.push(reviewer);
+    }
+}
+
+const pulls = [];
 
 (async () => {
     try {
         const BASE_URL = github.context.payload.repository.url;
+        // const BASE_URL = "https://api.github.com/repos/ygnoh/actions-tutorial";
         const fetchPulls = () => authFetch(`${BASE_URL}/pulls`);
         const fetchReviewers = number => authFetch(`${BASE_URL}/pulls/${number}/requested_reviewers`)
             .then(({users/* , teams */}) => users); // 팀 단위로 리뷰를 요청한 경우는 고려하지 않는다
         const fetchUser = url => authFetch(url);
-        const pulls = await fetchPulls();
 
-        for (const pull of pulls) {
-            const {number} = pull;
-            const users = await fetchReviewers(number);
+        for (const pullInfo of await fetchPulls()) {
+            const pull = new Pull(pullInfo);
 
-            for (const user of users) {
+            pulls.push(pull);
+
+            for (const user of await fetchReviewers(pull.number)) {
                 const {url} = user;
 
-                if (userUrlEmailPair[url]) {
-                    return;
-                }
-
-                const info = await fetchUser(url);
-                const {email} = info;
-
-                userUrlEmailPair[url] = email;
+                pull.addReviewer(await fetchUser(url));
             }
         }
 
-        console.log(JSON.stringify(userUrlEmailPair, undefined, 2));
+        console.log(pulls);
         // todo send notifications
     } catch (e) {
         core.setFailed(e.message);
