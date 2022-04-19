7079dafede1a212bb2ea6ebbf1b23460060e2cc0
yonggoo-noh <yonggoo.noh@navercorp.com>
2022-04-19 12:07:07 +0900

    Create messages using user-pr pairs

--- index.js
+++ index.js
@@ -9,36 +9,143 @@ const authFetch = url => axios({
     },
     url
 }).then(res => res.data);
-const sendSlack = (channel, text) => axios({
+const createRequestPRData = (user) => ({
+    text: "좋은 아침이에요 :wave:",
+    blocks: [
+        {
+            type: "section",
+            text: {
+                type: "mrkdwn",
+                text: "좋은 아침입니다 :wave:"
+            }
+        },
+        {
+            type: "section",
+            text: {
+                type: "mrkdwn",
+                text: `<@${user.name}> 님의 리뷰를 애타게 기다리는 :pray: 동료의 PR이 있어요. 리뷰에 참여해 주세요:`
+            }
+        },
+        {
+            type: "section",
+            text: {
+                type: "mrkdwn",
+                text: user.requestedPRs
+                    .map(({title, url}) => `• <${url}|${title}>`)
+                    .join("\n")
+            }
+        }
+    ]
+});
+/**
+ * @param {User} user
+ * @param {object} data
+ */
+const sendSlack = (user, data) => axios({
     method: "post",
     headers: {
         Authorization: `Bearer ${core.getInput("slackBotToken")}`,
         "Content-Type": "application/json"
     },
     url: "https://slack.com/api/chat.postMessage",
-    data: {channel, text}
+    data: {
+        channel: `@${user.name}`,
+        ...data
+    }
 });
 
 class Pull {
+    /**
+     * @type {{[key: string]: Pull}}
+     * @private
+     */
+    static _instances = {};
+
+    /**
+     * @param {{title: string, html_url: string, number: number}} pullInfo
+     * @returns {Pull}
+     */
+    static create(pullInfo) {
+        const {html_url: url} = pullInfo;
+
+        return Pull._instances[url] || (Pull._instances[url] = new Pull(pullInfo));
+    }
+
     constructor(pullInfo) {
         const {title, html_url, number} = pullInfo;
 
         this._title = title;
         this._url = html_url;
         this._number = number;
-        this._reviewers = [];
+    }
+
+    get title() {
+        return this._title;
+    }
+
+    get url() {
+        return this._url;
     }
 
     get number() {
         return this._number;
     }
-
-    addReviewer(reviewer) {
-        this._reviewers.push(reviewer);
-    }
 }
 
-const pulls = [];
+class User {
+    /**
+     * @returns {User[]}
+     */
+    static getUsers() {
+        return Object.values(User._instances);
+    }
+
+    /**
+     * @type {{[key: string]: User}}
+     * @private
+     */
+    static _instances = {};
+
+    /**
+     * @param {{email: string}} userInfo
+     * @returns {User}
+     */
+    static create(userInfo) {
+        const {email} = userInfo;
+
+        return User._instances[email] || (User._instances[email] = new User(userInfo));
+    }
+
+    constructor(userInfo) {
+        const {email} = userInfo;
+
+        /**
+         * @type {string}
+         * @private
+         */
+        this._email = email;
+        /**
+         * @type {Pull[]}
+         * @private
+         */
+        this._requestedPRs = [];
+    }
+
+    get name() {
+        return this._email?.split("@")[0];
+    }
+
+    get requestedPRs() {
+        return this._requestedPRs;
+    }
+
+    /**
+     * @param {Pull} pull
+     */
+    requestReview(pull) {
+        this._requestedPRs.push(pull);
+    }
+}
 
 (async () => {
     try {
@@ -50,18 +157,25 @@ const pulls = [];
         const fetchUser = url => authFetch(url);
 
         for (const pullInfo of await fetchPulls()) {
-            const pull = new Pull(pullInfo);
+            const pull = Pull.create(pullInfo);
 
-            pulls.push(pull);
+            for (const userInfo of await fetchReviewers(pull.number)) {
+                const user = User.create(await fetchUser(userInfo.url));
 
-            for (const user of await fetchReviewers(pull.number)) {
-                const {url} = user;
-
-                pull.addReviewer(await fetchUser(url));
+                user.requestReview(pull);
             }
         }
 
-        sendSlack("@yonggoo.noh", "HELLO!!"); // todo replace with user email
+        const users = User.getUsers();
+
+        users.forEach(user => {
+            if (!user.name) {
+                return; // oss public email 미등록 사용자
+            }
+
+            sendSlack(user, createRequestPRData(user));
+        });
+
         core.info("Messages sent");
     } catch (e) {
         core.setFailed(e.message);
